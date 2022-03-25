b43f8165f8d087e5cad7fb468bc18da88076501e
luanalimadeveloper <luanalima.developer@gmail.com>
2022-03-25 20:38:00 -0300

    Client entity

---
+++ backend/src/main/java/com/luanasproject/Myproject/entities/Client.java
@@ -0,0 +1,96 @@
+package com.luanasproject.Myproject.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthdate;
+	private Integer children;
+
+	public Client() {
+		
+	}
+
+	public Client(Long id, String name, String cpf, Double income, Instant birthdate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthdate = birthdate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Instant birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
